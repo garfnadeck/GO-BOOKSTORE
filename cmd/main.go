b65ace2afcf77bb2d01e708b9253b780df81eb96
garfnadeck <serraopinto@gmail.com>
2023-09-19 00:12:12 -0300

    Bookstore using go

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"GO-BOOKSTORE/pkg/routes"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("Localhost:9010", r))
+}
